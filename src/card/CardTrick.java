19d0d0ba17c15fd4b3fabe8bb05e858be459d3ba
kachhiyz <kachhiyz@sheridancollege.ca>
2024-09-19 21:16:35 -0400

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -59,6 +59,26 @@ public class CardTrick {
                 break;
             }
         }
+                        // Hard-coded lucky card
+        Card luckyCard = new Card();
+        luckyCard.setValue(7); // Lucky card value: 7
+        luckyCard.setSuit("Hearts"); // Lucky card suit: Hearts
+        
+        boolean luckyCardFound = false;
+        for (Card c : magicHand) {
+        if (c.getValue() == luckyCard.getValue() && c.getSuit().equals(luckyCard.getSuit())) {
+        luckyCardFound = true;
+        break;
+    }
+}
+
+
+    if (luckyCardFound) {
+        System.out.println("The lucky card (7 of Hearts) is in the magic hand!");
+    } else {
+        System.out.println("The lucky card (7 of Hearts) is not in the magic hand.");
+    }
+
         
         // Report the result
         if (cardFound) {
